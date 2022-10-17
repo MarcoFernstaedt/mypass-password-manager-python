102f3179665f8d8ac2017fdc63795fed371d03c2
Marco <fernstaedtmarco@gmail.com>
2022-10-17 14:48:58 -0700

    add button now can save to a file named data.txt

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from tkinter import *
 
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -14,12 +25,14 @@ website_label.grid(column=0, row=1)
 
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 email_label = Label(text="Email/Username: ")
 email_label.grid(column=0, row=2, columnspan=2)
 
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2)
+email_entry.insert(0, "example@email.com")
 
 password_label = Label(text="Password: ")
 password_label.grid(column=0, row=3)
@@ -30,7 +43,7 @@ password_entry.grid(column=1, row=3)
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3, columnspan=2)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
